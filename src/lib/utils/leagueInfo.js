2be955fe9cf8396d9f1cb7d6ce3ab7858e8c4ea1
jbergs31 <joshberger35@gmail.com>
2024-11-03 11:15:02 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to the GOA Constrictors League Page!.</p>
+  <p>Welcome to the GOA Constrictors League Page!</p>
 `;
 
 /*   STEP 3   */
